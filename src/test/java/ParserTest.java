b2d40a04eacede9b3fae662ea226c0c025f26610
skagelsGM <skagels@gmail.com>
2016-03-03 09:50:27 -0500

    test code cleanup

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -4,9 +4,9 @@ import static org.junit.Assert.*;
 
 public class ParserTest {
 
-	private static final String BANANA_INPUT_FILE = "/Users/scott/dev/projects/datto/input/banana.txt";
-	private static final String PEOPLE_INPUT_FILE = "/Users/scott/dev/projects/datto/input/people.txt";
-	private static final String WHEREFORE_INPUT_FILE = "/Users/scott/dev/projects/datto/input/wherefore.txt";
+	// file paths assume that test is executed in the project root folder (..../datto/)
+	private static final String PEOPLE_INPUT_FILE = "./input/people.txt";
+	private static final String WHEREFORE_INPUT_FILE = "./input/wherefore.txt";
 
 	@Test public void tesMaxRepeatingLetter() {
 		Parser parser = new Parser();
@@ -16,8 +16,7 @@ public class ParserTest {
 	
 	@Test public void testFindWordWithMaxRepeatingLetter() {
 		Parser parser = new Parser();
-		assertEquals( "banana", parser.findWordWithMaxRepeatingLetter(BANANA_INPUT_FILE) );
 		assertEquals( "people", parser.findWordWithMaxRepeatingLetter(PEOPLE_INPUT_FILE) );
-		assertEquals( "wherefore",  parser.findWordWithMaxRepeatingLetter(WHEREFORE_INPUT_FILE) );
+		assertEquals( "wherefore", parser.findWordWithMaxRepeatingLetter(WHEREFORE_INPUT_FILE) );
 	}
 }
